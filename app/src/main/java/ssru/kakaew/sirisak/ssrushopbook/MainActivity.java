0b2e635b4f5094d4fe2c0c41d224c29f59af10a8
Pae <golf_skt01@hotmail.com>
2016-05-31 14:00:19 +0700

    JSON

--- app/src/main/java/ssru/kakaew/sirisak/ssrushopbook/MainActivity.java
+++ app/src/main/java/ssru/kakaew/sirisak/ssrushopbook/MainActivity.java
@@ -1,17 +1,25 @@
 package ssru.kakaew.sirisak.ssrushopbook;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 
 public class MainActivity extends ActionBarActivity {
 
     //Explicit
     private MyManage myManage;
+    private static final String urlJSON = "http://swiftcodingthai.com/ssru/get_user_Pae.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +29,68 @@ public class MainActivity extends ActionBarActivity {
         //Request SQLite
         myManage = new MyManage(MainActivity.this);
 
+        //Test Add Value to SQLite
+        //myManage.addNewUser("Name", "sur", "user", "password", "money");
+
+        //Delete All userTABLE
+        deleteAlluserTABLE();
+
+        synJSONtoSQLite();
+
     } // Main Method
+
+    private void synJSONtoSQLite() {
+
+        ConnectedUseTABLE connectedUseTABLE = new ConnectedUseTABLE();
+        connectedUseTABLE.execute();
+
+    }
+
+    public class ConnectedUseTABLE extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("31May", "My ERROR ==> " + e.toString());
+                return null;
+            }
+
+        } // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+
+                Log.d("31May", "JSON ==>" + s);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+        } //OnPost
+
+
+    } // Connect Class
+
+    private void deleteAlluserTABLE() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
--- app/src/main/java/ssru/kakaew/sirisak/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/kakaew/sirisak/ssrushopbook/SignUpActivity.java
@@ -22,7 +22,7 @@ public class SignUpActivity extends AppCompatActivity {
             userEditText, passwordEditText;
     private String nameString, surnameString,
             userString, passwordString;
-    private static final String urlUpload = "http://swiftcodingthai.com/ssru/add_user_Pae.php";
+    private static final String urlUpload = "http://swiftcodingthai.com/ssru/add_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +72,7 @@ public class SignUpActivity extends AppCompatActivity {
                 .add("Password", passwordString)
                 .build();
         Request.Builder builder = new Request.Builder();
-        Request request = builder.url("http://swiftcodingthai.com/ssru/add_user_Pae.php").post(requestBody).build();
+        Request request = builder.url(urlUpload).post(requestBody).build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
